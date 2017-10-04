3261fc212ae2d9288f3dd82190ed53c778333984
Harrison Miller <harrison.miller@yale.edu>
2017-10-04 04:58:24 -0700

    rename redux store file

--- App.js
+++ App.js
@@ -4,7 +4,7 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import Application from './js/redux/containers/Application';
-import store from './js/redux';
+import store from './js/redux/store';
 
 export default class App extends Component {
   render() {
